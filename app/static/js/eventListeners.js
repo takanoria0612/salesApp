cfb541f831403b8f364730b457d19f101353217a
USD Taka <takanorihiraki06@gmail.com>
2024-02-21 19:38:16 +0900

    eventListenerの修正

---
+++ app/static/js/eventListeners.js
@@ -0,0 +1,33 @@
+// eventListeners.js
+import { setFormReadOnly, updateFormData, updateFinancials } from './formUtils.js';
+import { fetchHolidays } from './holidays.js';
+import { handleDateChange } from './dateHandlers.js';
+
+export default async function setupEventListeners() {
+    // 祝日データをフェッチして、グローバル変数または状態管理に保存
+    let holidays = {};
+    try {
+        holidays = await fetchHolidays();
+    } catch (error) {
+        console.error('Failed to fetch holidays:', error);
+        // エラーハンドリング: ユーザーへの通知、デフォルト値の設定など
+    }
+
+    // 日付フィールドに対するイベントリスナー
+    document.getElementById('date').addEventListener('change', handleDateChange)
+    document.getElementById('card_total').addEventListener('input', updateFinancials);
+    document.getElementById('usd_total').addEventListener('input', updateFinancials);
+    document.getElementById('customers').addEventListener('input', updateFinancials);
+    document.getElementById('cash_total').addEventListener('input', updateFinancials);
+
+
+    // フォーム送信時のイベントリスナー
+    document.querySelector('form').addEventListener('submit', async (e) => {
+        e.preventDefault(); // デフォルトのフォーム送信を防止
+        await updateFinancials(); // 財務データの最終更新
+        // ここで、フォームデータの送信やその他の送信前処理を行う
+
+    });
+}
+// ページロード時にイベントリスナーをセットアップ
+document.addEventListener('DOMContentLoaded', setupEventListeners);
