4ef093baf44c07df2cd588ac46e3806e5b317e23
USD Taka <takanorihiraki06@gmail.com>
2024-02-22 14:22:42 +0900

    微調整

--- app/utils/user_utils.py
+++ app/utils/user_utils.py
@@ -1,15 +1,3 @@
-# import os
-# from typing import Dict
-
-# def load_user_from_env() -> Dict[str, Dict]:
-#     user_database = {}
-#     user_count = int(os.getenv('USER_COUNT', 0))  # USER_COUNT 環境変数で管理されるユーザーの数
-#     for i in range(1, user_count + 1):
-#         username = os.getenv(f'USERNAME{i}')
-#         password = os.getenv(f'PASSWORD{i}')
-#         if username and password:
-#             user_database[username] = {"id": str(i), "username": username, "password": password}
-#     return user_database
 import os
 import json
 from typing import Dict
